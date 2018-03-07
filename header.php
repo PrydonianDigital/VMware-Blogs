65dca82acfa2c475790146f64938a73c7d18cc8c
vbloke <vbloke@gmail.com>
2018-03-07 06:57:49 +0000

    small amends

--- header.php
+++ header.php
@@ -51,7 +51,7 @@
 			<?php
 				wp_nav_menu(array(
 					'container'			=> false,
-							'menu'				=> __( 'Priorities Menu', 'vmw' ),
+					'menu'				=> __( 'Priorities Menu', 'vmw' ),
 					'menu_class'		=> 'vertical menu',
 					'theme_location'	=> 'solutions',
 				    'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
